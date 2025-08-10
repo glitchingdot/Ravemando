5385693493de460671f8735117e7a2bc5da0b9e3
glitchingdot <jack@glitching.xyz>
2025-08-10 01:32:04 -0700

    Added Loader and Classic (untested)

--- Main/RavemandoPlugin.cs
+++ Main/RavemandoPlugin.cs
@@ -6,16 +6,10 @@ using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
-using IL.RoR2.ContentManagement;
-using MonoMod.RuntimeDetour.HookGen;
 using R2API;
 using RoR2;
-
-using RiskOfOptions;
-
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using RiskOfOptions.Options;
 
 namespace Ravemando
 {
@@ -214,6 +208,8 @@ namespace Ravemando
 
             AddRavemando();
             AddHornet();
+            AddLoader();
+            AddClassic();
             AddTraptain();
             AddRadmiral();
 
@@ -382,6 +378,86 @@ namespace Ravemando
             AddSkinToSkinController(skinController, skinDefInfo);
         }
 
+        private static void AddLoader()
+        {
+            string bodyPrefabName = "LoaderBody";
+            string skinName = "L0@d3r";
+            string skinNameToken = "JACKDOTPNG_SKIN_COMMANDO_-_LOADER_NAME";
+
+            Sprite icon = assetBundle.LoadAsset<Sprite>("Assets/Placeholder Icon.png");
+
+            int baseSkinIndex = 0;
+
+            GameObject bodyPrefab = null;
+            GameObject modelTransform = null;
+            ModelSkinController skinController = null;
+
+            SkinDefInfo skinDefInfo = CreateNewSkinDefInfo(bodyPrefabName, skinName, skinNameToken, icon, baseSkinIndex, out bodyPrefab, out modelTransform, out skinController);
+
+            CharacterModel.RendererInfo[] newRendererInfos = new CharacterModel.RendererInfo[1];
+            Renderer[] renderers = modelTransform.GetComponentsInChildren<Renderer>(true);
+
+            Material loadedMat = Addressables.LoadAssetAsync<Material>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Loader.matLoaderPilotDiffuse_mat).WaitForCompletion();
+            Material instancedMat = new Material(loadedMat);
+
+            InstanceLogger.LogInfo("Loaded material: " + instancedMat.name);
+
+            int rendererIndex = 2;
+
+            newRendererInfos[0] = new CharacterModel.RendererInfo
+            {
+                defaultMaterial = instancedMat,
+                defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                ignoreOverlays = false,
+                renderer = renderers[rendererIndex],
+            };
+
+            AddToCycle(newRendererInfos[0]);
+            skinDefInfo.RendererInfos = newRendererInfos;
+
+            AddSkinToSkinController(skinController, skinDefInfo);
+        }
+
+        private static void AddClassic()
+        {
+            string bodyPrefabName = "LoaderBody";
+            string skinName = "ClA$$ic";
+            string skinNameToken = "JACKDOTPNG_SKIN_COMMANDO_-_CLASSIC_NAME";
+
+            Sprite icon = assetBundle.LoadAsset<Sprite>("Assets/Placeholder Icon.png");
+
+            int baseSkinIndex = 1;
+
+            GameObject bodyPrefab = null;
+            GameObject modelTransform = null;
+            ModelSkinController skinController = null;
+
+            SkinDefInfo skinDefInfo = CreateNewSkinDefInfo(bodyPrefabName, skinName, skinNameToken, icon, baseSkinIndex, out bodyPrefab, out modelTransform, out skinController);
+
+            CharacterModel.RendererInfo[] newRendererInfos = new CharacterModel.RendererInfo[1];
+            Renderer[] renderers = modelTransform.GetComponentsInChildren<Renderer>(true);
+
+            Material loadedMat = Addressables.LoadAssetAsync<Material>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Loader.matLoaderPilotDiffuse_mat).WaitForCompletion();
+            Material instancedMat = new Material(loadedMat);
+
+            InstanceLogger.LogInfo("Loaded material: " + instancedMat.name);
+
+            int rendererIndex = 2;
+
+            newRendererInfos[0] = new CharacterModel.RendererInfo
+            {
+                defaultMaterial = instancedMat,
+                defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                ignoreOverlays = false,
+                renderer = renderers[rendererIndex],
+            };
+
+            AddToCycle(newRendererInfos[0]);
+            skinDefInfo.RendererInfos = newRendererInfos;
+
+            AddSkinToSkinController(skinController, skinDefInfo);
+        }
+
         private static void AddTraptain()
         {
             string bodyPrefabName = "CaptainBody";
@@ -441,7 +517,7 @@ namespace Ravemando
             CharacterModel.RendererInfo[] newRendererInfos = new CharacterModel.RendererInfo[1];
             Renderer[] renderers = modelTransform.GetComponentsInChildren<Renderer>(true);
 
-            Material loadedMat = Addressables.LoadAssetAsync<Material>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Captain.matCaptainRobotBitsAlt_mat).WaitForCompletion();
+            Material loadedMat = Addressables.LoadAssetAsync<Material>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Loader.matLoaderPilotDiffuseAlt_mat).WaitForCompletion();
             Material instancedMat = new Material(loadedMat);
 
             InstanceLogger.LogInfo("Loaded material: " + instancedMat.name);
